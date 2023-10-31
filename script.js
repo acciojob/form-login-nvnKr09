df6a0a264940c27ea7fe9e10d753e8154c282fca
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2023-10-31 21:09:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	const Fname = document.querySelector('input[name="fname"]').value;
+	const Lname = document.querySelector('input[name="lname"]').value;
 
+	alert(`${Fname} ${Lname}`);
 }
